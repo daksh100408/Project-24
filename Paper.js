d4c7d4a13ead96329122b6dbdd1423d97e42b45e
daksh100408 <69684809+daksh100408@users.noreply.github.com>
2020-08-30 17:16:39 +0800

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,23 @@
+class Paper{
+constructor(x, y, radius) {
+    var options = {
+        isStatics:false,
+        'restitution':0.3,
+        'friction':0.5,
+        'density':1.2
+    }
+
+this.body = Bodies.circle(x, y, radius, options);
+this.radius = radius;
+}
+
+display(){
+var pos = this.body.position;    
+var angle = this.body.angle;
+push();
+translate(this.body.position.x, this.body.position.y);
+rotate(angle);
+pop();
+}
+
+}
